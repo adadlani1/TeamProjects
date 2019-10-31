90aebfcce18d3ba2b0114925b8fe6c58282d1afc
ayo <au_salako@hotmail.com>
2019-10-31 20:46:01 +0000

    json data

--- helpdesk/phase-1-without-frameworks/src/app.js
+++ helpdesk/phase-1-without-frameworks/src/app.js
@@ -1,7 +1,16 @@
-alert("hello");
+//alert("hello");
 function myDummyFunction(){
     var v = $('#call-number').val()
     alert(v)
 }
 
-myDummyFunction()
+//myDummyFunction()
+var client = new XMLHttpRequest();
+client.open('GET', '}http://localhost:63342/TeamProjects/helpdesk/phase-1-without-frameworks/src/data');
+client.onreadystatechange = function() {
+    alert(client.responseText);
+}
+client.send();
+
+
+
