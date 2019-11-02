01730d3e5770b8ae83c63f4839f5cd026ecaf3fa
Anmol Dadlani <anmoldadlani@gmail.com>
2019-11-02 11:38:50 +0000

    modal works for refer to specialist on new-issue

--- helpdesk/phase-1-without-frameworks/src/app.js
+++ helpdesk/phase-1-without-frameworks/src/app.js
@@ -13,3 +13,32 @@ $(document).ready(function () {
             $(`#tasks-${specialist_id}`).append("</br><p>ID-421</p><p>Problem with printers</p>")
     });
 });
+
+$(document).ready(function () {
+    var id = Math.floor(Math.random() * 1000);
+    $('#problem-id').append(id);
+
+    var currentDate = new Date(),
+        day = currentDate.getDate(),
+        month = currentDate.getMonth() + 1,
+        year = currentDate.getFullYear();
+
+    if(day<10){
+        var newday = "0" + day;
+    }
+    var date = newday + "/" + month + "/" + year;
+
+    var currentTime = new Date(),
+        hours = currentTime.getHours(),
+        minutes = currentTime.getMinutes();
+
+    if (minutes < 10) {
+        minutes = "0" + minutes;
+    }
+
+    var time = hours + ":" + minutes;
+
+    var dateTime = date + " " + time;
+    $('#dateTime').append(dateTime);
+});
+
