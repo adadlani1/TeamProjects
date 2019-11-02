aab955681bf0c98af0aa6ef92bd451e476ef6b9a
Anmol Dadlani <anmoldadlani@gmail.com>
2019-11-02 21:23:07 +0000

    sorted layout for login page

--- helpdesk/phase-1-without-frameworks/src/loginScript.js
+++ helpdesk/phase-1-without-frameworks/src/loginScript.js
@@ -6,27 +6,28 @@ const Roles = [{
   username: 'bert',
   password: 'bert',
   role: 'Specialist'
-}]
+}];
 
 function validate(form) {
-  event.preventDefault()
-  const username = ($('#username').val())
-  const password = ($('#password').val())
+  event.preventDefault();
+  const username = ($('#username').val());
+  const password = ($('#password').val());
+  console.log(username + password);
   const user = Roles.filter((role) => {
     if (role.username === username.toLowerCase().trim() && role.password === password) {
-      console.log('Matched')
+      console.log('Matched');
       return true
     }
-  })
+  });
   if (!user.length) {
-    $('#error').empty()
+    $('#error').empty();
     $('#error').append('Wrong Details')
   }
   else {
     if (user[0].role === 'Operator')
-      window.location.href = '/helpdesk/phase-1-without-frameworks/dashboard.html'
+      window.location.href = 'dashboard.html';
     if (user[0].role === 'Specialist')
-      window.location.href = '/helpdesk/phase-1-without-frameworks/specialist-dashboard.html'
+      window.location.href = 'specialist-dashboard.html'
   }
 
 }
